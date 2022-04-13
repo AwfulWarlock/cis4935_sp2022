778d42487b6f836ff3ac165c502bd55c89298ed9
AwfulWarlock <95302091+AwfulWarlock@users.noreply.github.com>
2022-04-13 12:53:30 -0400

    Update interactive_curl.sh

--- SP_SP2022_Scripts/interactive_curl.sh
+++ SP_SP2022_Scripts/interactive_curl.sh
@@ -9,10 +9,10 @@ then
     read -p "Num to run in parallel: " num
     if (( $choice == 1)) 
     then
-	time (xargs -I % -P $num -n 1 curl -ks -o /dev/null --http3  "https://172.31.81.42/" < <(printf '%s\n' {1..$num}))
+	{ time (xargs -I % -P $num -n 1 curl -ks -o /dev/null --http3  "https://172.31.81.42/" < <(printf '%s\n' {1..$num})) } 2> $HOME/Desktop/runtime.log
     else
 	#xargs -I % -P $num -n 1 curl -ks -o /dev/null --http3 "https://172.31.81.42/img/car.jpg" < <(printf '%s\n' {1..$num})
-	time (seq 1 $num | xargs -n 1 -P $num curl --http3 https://172.31.81.42/img/image.jpg -ks -o /dev/null)
+	{ time (seq 1 $num | xargs -n 1 -P $num curl --http3 https://172.31.81.42/img/image.jpg -ks -o /dev/null) } 2> $HOME/Desktop/runtime.log
     fi # Inner IF
 else
     echo "You chose HTTP2 ya goober"
@@ -20,9 +20,9 @@ else
     read -p "Num to run in parallel: " num
     if (( $choice == 1)) 
     then
-	time (xargs -I % -P $num -n 1 curl -ks --max-time 5 -o /dev/null --http2  "https://172.31.81.42/" < <(printf '%s\n' {1..$num}))
+	{ time (xargs -I % -P $num -n 1 curl -ks --max-time 5 -o /dev/null --http2  "https://172.31.81.42/" < <(printf '%s\n' {1..$num})) } 2> $HOME/Desktop/runtime.log
     else
-	time (seq 1 $num | xargs -n 1 -P $num curl --http2 https://172.31.81.42/img/image.jpg -ks -o /dev/null)
+	{ time (seq 1 $num | xargs -n 1 -P $num curl --http2 https://172.31.81.42/img/image.jpg -ks -o /dev/null) } 2> $HOME/Desktop/runtime.log
 	#xargs -I % -P $num -n 1 curl -ks --max-time 5 -o /dev/null --http2  "https://172.31.81.42/img/car.jpg" < <(printf '%s\n' {1..$num})
     fi # Inner IF
 fi # Outter IF
